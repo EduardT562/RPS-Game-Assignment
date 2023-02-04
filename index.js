3bade4d45881c4fe4c53e1444a3b55a634876c5b
Jeremías Rombolá <jere.rombola@gmail.com>
2023-02-04 13:07:08 -0300

    It was realized the logic of the functions and the results conditional statments

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const selection = ['Rock', 'Paper', 'Scissors'];
-const playerSelection = function () {};
-const computerSelection = function () {};
+const selection = ['Rock','Paper','Scissors'];
+const playerSelection = function () {
+    return prompt()
+};
 
-function playRound(playerSelection, computerSelection) {}
+const computerSelection = function () {
+    let random = Math.floor(Math.random()* selection.length) 
+    return selection[random]
+};
+
+let player = (playerSelection())
+let computer = (computerSelection())
+alert(cs)
+
+if (ps.toLowerCase() === cs.toLowerCase()) {
+     alert("You tie!") 
+    } else if ((ps.toLowerCase() === "rock" && cs.toLowerCase() === "paper") || (ps.toLowerCase() === "paper" && cs.toLowerCase() === "scissors") || (ps.toLowerCase() === "scissors" && cs.toLowerCase() == "rock"))
+    {
+    alert("You Lose!")  
+    } else {
+    alert("You win!")    
+    }
+
+const game = function (playerSelection,computerSelection){
+
+}
